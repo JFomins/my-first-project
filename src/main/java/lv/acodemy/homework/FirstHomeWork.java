3b482aca4f3c04a894950e6806261005c0aa6cf7
J.Fomins <jevgenijs_fomins@inbox.lv>
2023-03-08 18:46:40 +0200

    [classroom_01.03.2023]: Add FirstHomeWork.java

---
+++ src/main/java/lv/acodemy/homework/FirstHomeWork.java
@@ -0,0 +1,100 @@
+package lv.acodemy.homework;
+
+import java.util.Arrays;
+
+public class FirstHomeWork {
+    public static void main(String[] args) {
+
+        // byte data type
+
+        byte myCurrentAge = 32;
+        System.out.println(myCurrentAge);
+        byte myDogCurrentAge = 7;
+        System.out.println(myDogCurrentAge);
+
+        // short data type
+        short rentPerMonth = 500;
+        System.out.println(rentPerMonth);
+        short educationPricePerYear = 2000;
+        System.out.println(educationPricePerYear);
+
+
+        // char data type
+        char firstNameLetter = 'J';
+        System.out.println(firstNameLetter);
+        char secondNameLetter = 'F';
+        System.out.println(secondNameLetter);
+
+
+        // int data type
+        int howManyPeopleUseAndroid = 1000000;
+        System.out.println(howManyPeopleUseAndroid);
+        int howManyPeopleUseIos = 2000000;
+        System.out.println(howManyPeopleUseIos);
+
+
+        // long data type
+        long populationOfEurope = 746000000;
+        System.out.println(populationOfEurope);
+        long populationOfUSA = 331000000;
+        System.out.println(populationOfUSA);
+
+
+        // float data type
+        float worldRecordOfRunning100m = 9.58f;
+        System.out.println(worldRecordOfRunning100m);
+        float worldRecordOfSwimming100m = 46.91f;
+        System.out.println(worldRecordOfSwimming100m);
+
+
+        // boolean is; has;
+        //oolean isMonday = false;
+        //oolean isTuesday = true;
+
+
+        // String
+        String name = "Jevgenijs";
+        String surName = "Fomins";
+
+
+        // Concatenation
+        String fullName = name + " " + surName;
+        System.out.println(fullName);
+
+
+        String fullNameWithAge = fullName + myCurrentAge;
+        System.out.println(fullNameWithAge);
+
+
+        System.out.printf("My full name is: %s. I am %d years old.\n", fullName, myCurrentAge);
+
+
+        // /
+        int a = 100;
+        int b = 20;
+        int c = a / b;
+        System.out.println(c);
+
+
+        int d = 100 % 20;
+        System.out.println(d);
+        int g = 50 % 10;
+        System.out.println(g);
+    }
+}
+
+
+
+
+
+    // *
+    // int a = 10;
+    //int b = 20;
+    //int c = a * b;
+
+
+
+
+
+
+
